ffdd5c8c6181d365a5dbde1505fbfe6e06b4e208
Nathalia Bruno <nathi.bruno@gmail.com>
2023-02-07 13:37:13 -0300

    Add shopify polaris

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import "@shopify/polaris/build/esm/styles.css"
+import enTranslations from "@shopify/polaris/locales/en.json"
+import { AppProvider } from "@shopify/polaris"
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <AppProvider i18n={enTranslations}>
+      <Component {...pageProps} />
+    </AppProvider>
+  )
 }
