3e5ae9f9a4a9eaea534150e000e21d45b0c2083f
Nathalia Bruno <nathi.bruno@gmail.com>
2023-02-07 15:57:00 -0300

    Add store provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,16 @@ import type { AppProps } from 'next/app'
 import "@shopify/polaris/build/esm/styles.css"
 import enTranslations from "@shopify/polaris/locales/en.json"
 import { AppProvider } from "@shopify/polaris"
+import { Provider } from "react-redux"
+
+import { store } from "@/redux/store"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <AppProvider i18n={enTranslations}>
-      <Component {...pageProps} />
-    </AppProvider>
+    <Provider store={store}>
+      <AppProvider i18n={enTranslations}>
+        <Component {...pageProps} />
+      </AppProvider>
+    </Provider>
   )
 }
